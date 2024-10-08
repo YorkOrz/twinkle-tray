c24eac65dd486d1863ce1eb8beeec19ceac0085b
Xander Frangos <33106561+xanderfrangos@users.noreply.github.com>
2024-10-07 22:10:10 -0400

    Enable HDR sliders per display in settings

--- src/electron.js
+++ src/electron.js
@@ -433,6 +433,7 @@ const defaultSettings = {
   monitorFeatures: {},
   monitorFeaturesSettings: {},
   hideDisplays: {},
+  hdrDisplays: {},
   checkForUpdates: !isDev,
   dismissedUpdate: '',
   language: "system",
